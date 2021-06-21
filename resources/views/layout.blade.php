8bb65199b726db7890ac9f041070ef2eb41cc0ac
jessGra <jesus.egranada@gmail.com>
2021-06-21 13:49:11 -0500

    add favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,9 @@
     <title>@yield('title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- icon -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
      <!-- Scripts -->
